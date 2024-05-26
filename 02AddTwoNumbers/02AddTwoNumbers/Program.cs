f489ce7f6eb86297b48069276eb63ff5514e7950
Felipe Lira <filipi6219@gmail.com>
2024-05-25 22:03:50 -0300

    refactor: improve 02AddTwoNumbers

--- 02AddTwoNumbers/02AddTwoNumbers/Program.cs
+++ 02AddTwoNumbers/02AddTwoNumbers/Program.cs
@@ -7,36 +7,19 @@
 
         }
 
-        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2, int carry = 0)
         {
-            ListNode l3 = new ListNode();
-            ListNode currentNode = l3;
+            bool endOfListNode = l1 == null && l2 == null && carry == 0;
+            if (endOfListNode) return null;
 
-            while (l1 != null || l2 != null)
-            {
-                int l1Value = l1 != null ? l1.Val : 0;
-                int l2Value = l2 != null ? l2.Val : 0;
+            int l1Value = l1 != null ? l1.Val : 0;
+            int l2Value = l2 != null ? l2.Val : 0;
+            int total = l1Value + l2Value + carry;
 
-                int l3Value = l1Value + l2Value + currentNode.Val;
-                int l3ValueUnits = l3Value % 10;
-                int l3ValueTens = l3Value / 10;
+            int l3Value = total % 10;
+            carry = total / 10;
 
-                currentNode.Val = l3ValueUnits;
-                l1 = l1?.Next;
-                l2 = l2?.Next;
-
-                if (l1 != null || l2 != null)
-                {
-                    currentNode.Next = new ListNode(l3ValueTens);
-                    currentNode = currentNode.Next;
-                }
-                else if (l3ValueTens > 0)
-                {
-                    currentNode.Next = new ListNode(l3ValueTens);
-                }
-            }
-
-            return l3;
+            return new ListNode(l3Value, AddTwoNumbers(l1?.Next, l2?.Next, carry));
         }
     }
 }
