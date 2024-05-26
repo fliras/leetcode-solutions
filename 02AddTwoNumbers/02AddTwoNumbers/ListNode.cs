16837be998b0e999f60d4617ccf25af7dd9c8b95
Felipe Lira <filipi6219@gmail.com>
2024-05-25 21:46:19 -0300

    feat: add 02TwoNumbers

---
+++ 02AddTwoNumbers/02AddTwoNumbers/ListNode.cs
@@ -0,0 +1,14 @@
+﻿namespace _02AddTwoNumbers
+{
+    internal class ListNode
+    {
+        public int Val { get; set; }
+        public ListNode Next { get; set; }
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            Val = val;
+            Next = next;
+        }
+    }
+}
