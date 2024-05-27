0da9ffe5f764bd31ed14870246951c41ce7a027e
Felipe Lira <filipi6219@gmail.com>
2024-05-27 12:08:31 -0300

    feat: add 03LongestSubstringWithoutRepeatingCharacters

---
+++ 03LongestSubstringWithoutRepeatingCharacters/03LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03LongestSubstringWithoutRepeatingCharacters
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(LengthOfLongestSubstring("dvdf"));
+            Console.WriteLine(LengthOfLongestSubstring("bbbbbb"));
+            Console.WriteLine(LengthOfLongestSubstring("pwwkew"));
+            Console.ReadKey();
+        }
+
+        static int LengthOfLongestSubstring(string s)
+        {
+            int longestLength = 0;
+            int start = 0;
+            Dictionary<string, bool> substringChars = new Dictionary<string, bool>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                string letter = s[i].ToString();
+                if (substringChars.ContainsKey(letter))
+                {
+                    longestLength = Math.Max(longestLength, substringChars.Count);
+                    substringChars.Clear();
+                    i = start;
+                    start++;
+                }
+                else
+                {
+                    substringChars.Add(letter, true);
+                }
+            }
+
+            return Math.Max(longestLength, substringChars.Count);
+        }
+    }
+}
