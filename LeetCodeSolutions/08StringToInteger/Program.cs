326dfeca5375e7ee29a590952f7f3c9d889efdec
Felipe Lira <filipi6219@gmail.com>
2025-07-10 14:20:31 -0300

    refactor: quebra resolução do exercício 08 em métodos menores

---
+++ LeetCodeSolutions/08StringToInteger/Program.cs
@@ -0,0 +1,112 @@
+﻿namespace _08StringToInteger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"teu pai aquele corno: {MyAtoi(Console.ReadLine())}");
+            Console.ReadKey();
+        }
+
+        static int MyAtoi(string s)
+        {
+            string? numericString = ExtractValidNumericString(s);
+            if (numericString == null)
+                return 0;
+
+            return ParseStringToInteger(numericString);
+        }
+
+        static string? ExtractValidNumericString(string s)
+        {
+            string substringValue = string.Empty;
+
+            int i = 0;
+            bool shouldContinue = true;
+
+            while (shouldContinue && i < s.Length)
+            {
+                char currentChar = s[i++];
+
+                if (currentChar == ' ')
+                {
+                    if (!substringValue.Equals(string.Empty))
+                        shouldContinue = false;
+                }
+                else if (IsNumericSignal(currentChar))
+                {
+                    if (!substringValue.Equals(string.Empty))
+                        shouldContinue = false;
+                    else
+                        substringValue = currentChar.ToString();
+                }
+                else if (IsNumeric(currentChar))
+                {
+                    if (currentChar != '0')
+                        substringValue += currentChar.ToString();
+                    else if (substringValue.Equals(string.Empty))
+                        substringValue += currentChar.ToString();
+                    else if (substringValue.Length > 1 || (!IsNumericSignal(substringValue[0]) && substringValue[0] != '0'))
+                        substringValue += currentChar.ToString();
+                }
+                else
+                {
+                    shouldContinue = false;
+                }
+            }
+
+            return AdjustExtractedNumericString(substringValue);
+        }
+
+        static string? AdjustExtractedNumericString(string numericString)
+        {
+            if (numericString.Equals(string.Empty))
+                return null;
+
+            bool substringHasOnlyNumericSignal = numericString.Length == 1 && IsNumericSignal(numericString[0]);
+            if (substringHasOnlyNumericSignal)
+                return null;
+
+            bool numberIsExplicitlyPositive = numericString[0] == '+';
+            if (numberIsExplicitlyPositive)
+                return numericString[1..];
+
+            return numericString;
+        }
+
+        static int ParseStringToInteger(string s)
+        {
+            if (NumericStringOutOfIntSuperiorLimit(s))
+                return Int32.MaxValue;
+            
+            if (NumericStringOutOfIntInferiorLimit(s))
+                return Int32.MinValue;
+
+            return int.Parse(s);
+        }
+
+        static bool NumericStringOutOfIntSuperiorLimit(string s)
+        {
+            string maxIntStr = Int32.MaxValue.ToString();
+            bool valueIsPositive = s[0] != '-';
+            return valueIsPositive && (s.Length > maxIntStr.Length || (s.Length == maxIntStr.Length && String.Compare(s, maxIntStr) > 0));
+        }
+
+        static bool NumericStringOutOfIntInferiorLimit(string s)
+        {
+            string minIntStr = Int32.MinValue.ToString();
+            bool valueIsNegative = s[0] == '-';
+            return valueIsNegative && (s.Length > minIntStr.Length || (s.Length == minIntStr.Length && String.Compare(s, minIntStr) > 0));
+        }
+
+        static bool IsNumeric(char c)
+        {
+            return c >= 48 && c <= 57;
+        }
+
+        static bool IsNumericSignal(char c)
+        {
+            return c == '-' || c == '+';
+        }
+    }
+}
---
+++ LeetCodeSolutions/SimpleCalculator/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+
+            int num1 = ReadInt("Input the first number: ");
+            int num2 = ReadInt("Input the second number: ");
+
+            PrintMenu();
+            string choice = Console.ReadLine();
+            HandleOperation(choice, num1, num2);
+
+            Console.WriteLine("Press any key to quit...");
+            Console.ReadKey();
+        }
+
+        static int ReadInt(string prompt)
+        {
+            Console.WriteLine(prompt);
+            return int.Parse(Console.ReadLine());
+        }
+
+        static void PrintMenu()
+        {
+            Console.WriteLine("What do you want to do?");
+            Console.WriteLine("[A]dd numbers");
+            Console.WriteLine("[S]ubtract numbers");
+            Console.WriteLine("[M]ultiply numbers");
+        }
+
+        static void HandleOperation(string choice, int num1, int num2)
+        {
+            string upperChoice = choice.ToUpper();
+            if (upperChoice.Equals("A"))
+                PrintSum(num1, num2);
+            else if (upperChoice.Equals("S"))
+                PrintSubtraction(num1, num2);
+            else if (upperChoice.Equals("M"))
+                PrintMultiplication(num1, num2);
+            else
+                Console.WriteLine("Invalid choice!");
+        }
+
+        static void PrintSum(int num1, int num2)
+        {
+            Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+        }
+
+        static void PrintSubtraction(int num1, int num2)
+        {
+            Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+        }
+
+        static void PrintMultiplication(int  num1, int num2)
+        {
+            Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+        }
+    }
+}
